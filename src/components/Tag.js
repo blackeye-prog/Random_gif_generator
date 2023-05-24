dac9fa993883e410fc266cbddae882bea87c6181
Nilesh Srivastava <80592367+blackeye-prog@users.noreply.github.com>
2023-05-25 01:35:46 +0530

    Update Tag.js

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,11 +1,6 @@
 import React, { useState } from 'react'
-import axios from 'axios';
-import { useEffect } from 'react';
 import Spinner from './Spinner';
 import useGif from '../hooks/useGif';
-
-
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 const Tag = () => {
   const [tag, setTag] = useState('car');
